e332f3b0d81fb6946a42e48a2d032faa463f7265
Angel Sanchez <angeleduardosanchezgomez@gmail.com>
2021-03-17 20:36:42 -0600

    Modificacion de request XD

--- nodejs/request-handler.js
+++ nodejs/request-handler.js
@@ -1,34 +1,81 @@
-module.exports ={
-    mascotas: [
-        {tipo: "Perro", nombre: "Obha", dueno: "Arnulfo"},
-        {tipo: "Perro", nombre: "Luna", dueno: "Noraly"},
-        {tipo: "Pez", nombre: "Tiburoncin", dueno: "Angel"},
-        {tipo: "Pez", nombre: "Molly", dueno: "Angel"},
-        {tipo: "Gallo", nombre: "Chiquilin", dueno: "Ian"},
-    ],
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const enrutador = require("./enrutador");
 
-    veterinarias: [
-        {nombre: "Angel", apellido: "Sanchez", documento: "1234"},
-        {nombre: "Arnulfo", apellido: "Sanchez", documento: "4321"},
-        {nombre: "Ian", apellido: "Sanchez", documento: "2213"},
-        {nombre: "Noraly", apellido: "Nina", documento: "2604"}
-    ],
+module.exports= (req, res) => {
+    //Obtener url desde el objeto request
+    const urlActual = req.url;
+    const urlParseada = url.parse(urlActual, true);
 
-    duenos: [
-        {nombre: "Noraima", apellido: "Gomez", documento: "1302"},
-        {nombre: "Alberto", apellido: "Ochoa", documento: "1236"},
-        {nombre: "Jose", apellido: "Ortega", documento: "9874"},
-        {nombre: "Juan", apellido: "Perez", documento: "3456"}
-    ],
-    consultas: [
-        {
-            mascota: 0, 
-            veterinaria: 0, 
-            enzabezado: '', 
-            fechaCreacion: new Date(),
-            fechaEdicion: new Date(),
-            historia: '',
-            diagnostico: "",
-        },
-    ],
+    //Obtener la ruta
+    const ruta = urlParseada.pathname;
+    const rutaLimpia = ruta.replace(/^\/+|\/+$/g, "");
+
+   // obtener metodo http
+    const metodo = req.method.toLowerCase();
+
+    //Obtener las variables del query url
+    
+    const {query = {}}=urlParseada;
+
+    //Obtener los headers
+    const {headers = {}}=req;
+    //Obtener pay en el caso de haber uno
+    const decoder= new StringDecoder('utf-8');
+    let buffer = "";
+
+    //Ir acumulando la data cuando el request reciba un payload
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    //terminar de acumunar datos y finalizarlo
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        if (headers["content-type"] === "application/json") {
+            buffer = JSON.parse(buffer);
+        }
+
+          //3.4.3 revisar si tiene subrutas en este caso es el indice del array
+        
+        if (rutaLimpia.indexOf("/") > -1) {
+            var [rutaPrincipal, indice] = rutaLimpia.split("/");
+            //rutaLimpia = indice[0];
+            //indice = indice[1];
+        }
+
+        //ordenar data
+        const data ={
+            indice,
+            ruta: rutaPrincipal || rutaLimpia,
+            query,
+            metodo,
+            headers,
+            payload: buffer
+        };
+
+        console.log({data});
+        //Elegir el manejador de la ruta y asignar la funcion que el enrutador tiene
+        let handler;
+        if(data.ruta && enrutador[data.ruta] && enrutador[data.ruta][metodo]){
+            handler = enrutador[data.ruta][metodo];
+        }
+        else{
+            handler=enrutador.noEncontrado;
+        }
+
+        //ejecutar handler
+        if(typeof handler === 'function'){
+            handler(data, (statusCode=200, mensaje) => {
+                const respuesta = JSON.stringify(mensaje);
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(statusCode);
+
+                //linea donde se esta respondiendo a la aplicacion cliente
+                res.end(respuesta);
+            });
+        }
+
+    });
+
 };
